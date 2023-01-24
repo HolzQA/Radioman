703492c47ebd2415b768820da84ea89cdd607a5f
holz_qa <diatmin@gmail.com>
2023-01-24 23:48:31 +0300

    Add test-design and methods and tests

---
+++ src/main/java/netology/ru/Radiostation.java
@@ -0,0 +1,67 @@
+package netology.ru;
+
+public class Radiostation {
+    private int currentRadiostation;
+    private int currentVolume;
+
+    public int getCurrentRadiostation() {
+        return currentRadiostation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setNextRadiostation(int oldRadiostation) {
+        if (oldRadiostation = 9) {
+            currentRadiostation = 0;
+        } else {
+            currentRadiostation = oldRadiostation + 1;
+        }
+    }
+
+    public void setPrevRadiostation(int oldRadiostation) {
+        if (oldRadiostation = 0) {
+            currentRadiostation = 9;
+        } else {
+            currentRadiostation = oldRadiostation - 1;
+        }
+    }
+
+    public void setRadiostation(int newRadiostation) {
+        if (newRadiostation < 0) {
+            return;
+        }
+        if (newRadiostation > 9) {
+            return;
+        }
+        currentRadiostation = newRadiostation;
+    }
+
+    public void setNextVolume() {
+        if (currentVolume = 10) {
+            currentVolume = 0;
+        } else {
+            currentVolume = currentVolume + 1;
+        }
+    }
+
+    public void setPrevVolume() {
+        if (currentVolume = 0) {
+            currentVolume = 10;
+        } else {
+            currentVolume = currentVolume - 1;
+        }
+    }
+
+    public void setVolume(int newVolume) {
+        if (newVolume < 0) {
+            return;
+        }
+        if (newVolume > 10) {
+            return;
+        }
+        currentVolume = newVolume;
+    }
+
+}
---
+++ src/test/java/ru/netology/RadioTest.java
@@ -0,0 +1,90 @@
+package ru.netology;
+
+
+import netology.ru.Radiostation;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class RadioTest {
+    @ParameterizedTest
+        @CsvSource({,
+                 "9,0",
+                 "3,4"
+        })
+    public void testSetNextRadioctation(int curNum, int expexted) {
+        Radiostation radio = new Radiostation();
+
+        radio.setNextRadiostation(curNum);
+
+        Assertions.assertEquals(expexted, radio.getCurrentRadiostation());
+    }
+
+    @ParameterizedTest
+    @CsvSource({,
+            "0,9",
+            "5,4"
+    })
+    public void testSetPrevRadioctation(int curNum, int expexted) {
+        Radiostation radio = new Radiostation();
+
+        radio.setPrevRadiostation(curNum);
+
+        Assertions.assertEquals(expexted, radio.getCurrentRadiostation());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-1,0",
+            "3,3",
+            "10,0"
+    })
+    public void testSetRadiostation(int insert, int expected) {
+        Radiostation radio = new Radiostation();
+
+        radio.setRadiostation(insert);
+
+        Assertions.assertEquals(expected, radio.getCurrentRadiostation());
+    }
+
+    @ParameterizedTest
+    @CsvSource({,
+            "10,0",
+            "3,4"
+    })
+    public void testSetNextVolume(int curVol, int expexted) {
+        Radiostation radio = new Radiostation();
+
+        radio.setNextVolume(curVol);
+
+        Assertions.assertEquals(expexted, radio.getCurrentVolume());
+
+    }
+
+    @ParameterizedTest
+    @CsvSource({,
+            "0,10",
+            "5,4"
+    })
+    public void testSetPrevVolume(int curVol, int expexted) {
+        Radiostation radio = new Radiostation();
+
+        radio.setPrevVolume(curVol);
+
+        Assertions.assertEquals(expexted, radio.getCurrentVolume());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "-1,0",
+            "3,3",
+            "10,0"
+    })
+    public void testSetVolume(int insert, int expected) {
+        Radiostation radio = new Radiostation();
+
+        radio.setVolume(insert);
+
+        Assertions.assertEquals(expected, radio.getCurrentVolume());
+    }
+}
